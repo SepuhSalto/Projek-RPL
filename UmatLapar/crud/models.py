89529da0ef06388b8b8a8695208c5be8ab532267
SepuhSalto <mirzaalifahlevi@gmail.com>
2024-12-09 21:56:19 +0800

    buat database baru

--- UmatLapar/crud/models.py
+++ UmatLapar/crud/models.py
@@ -12,4 +12,13 @@ class Users(models.Model):
     def __str__(self):
         return self.name
 
+class makananrestoran(models.Model):
+    NamaMakanan = models.CharField(max_length=100)
+    HargaMakanan = models.DecimalField(max_digits=10, decimal_places=2)
 
+class restoran(models.Model):
+    namarestoran = models.CharField(max_length=100)
+    alamatrestoran = models.CharField(max_length=200)
+
+    def _str_(self):
+        return self.name
